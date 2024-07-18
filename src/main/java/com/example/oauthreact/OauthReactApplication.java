1b3a0f033a1cb4a38da5480e4695ad2ab621c351
postwo <codo7717@naver.com>
2024-07-19 06:48:23 +0900

    기setting

---
+++ src/main/java/com/example/oauthreact/OauthReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauthreact/OauthReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
